cbaf5852da5116f86e866a74d6afe1e1d7cea243
alexia <alexia_jg@outlook.es>
2023-06-13 20:33:31 -0500

    Initial commit

---
+++ src/main/java/com/example/lab8_20202330/Lab820202330Application.java
@@ -0,0 +1,13 @@
+package com.example.lab8_20202330;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab820202330Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab820202330Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab8_20202330/Lab820202330ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab8_20202330;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab820202330ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
